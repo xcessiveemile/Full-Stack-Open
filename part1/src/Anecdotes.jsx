14ad862466231959851864c2f49bd2ce16dca836
xcessiveemile <xcessiveemile@users.noreply.github.com>
2026-08-18 18:31:25 +0200

    Complete all Part 1 exercises (1.1 - 1.14)

--- part1/src/Anecdotes.jsx
+++ part1/src/Anecdotes.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 
-const Anecdotes = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time. The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  ]
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time. The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'The best time to plant a tree was 20 years ago. The second best time is now.',
+  'Programming without an extremely heavy test suite is like playing a game without a scoreboard.',
+]
 
+const Anecdotes = () => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const nextAnecdote = () => {
-    const nextIndex = Math.floor(Math.random() * anecdotes.length)
-    setSelected(nextIndex)
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
 
   const voteForSelected = () => {
