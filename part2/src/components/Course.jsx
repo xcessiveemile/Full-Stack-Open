e9395970bb406060ba89dcb677d4a999aca3d871
xcessiveemile <xcessiveemile@users.noreply.github.com>
2026-08-19 16:11:27 +0200

    Practice rerun: Manually reconstructed exercises 2.1 to 2.5 from scratch

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,39 +1,30 @@
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>
-}
+const Header = ({ name }) => <h2>{name}</h2>
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  )
-}
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
 
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
   return (
-    <p>
-      <strong>total of {total} exercises</strong>
-    </p>
+    <p><strong>total of {total} exercises</strong></p>
   )
 }
 
 const Course = ({ course }) => {
   return (
     <div>
-      <Header course={course} />
+      <Header name={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
