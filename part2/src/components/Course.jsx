1f2117a79efcdfd847d4e48b37c2fddb9756bd27
xcessiveemile <xcessiveemile@users.noreply.github.com>
2026-08-19 15:15:07 +0200

    Complete exercises 2.1 to 2.5

---
+++ part2/src/components/Course.jsx
@@ -0,0 +1,43 @@
+const Header = ({ course }) => {
+  return <h2>{course.name}</h2>
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
